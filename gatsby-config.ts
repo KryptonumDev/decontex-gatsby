3fc11e911e365400c454f4006bcc8ce3a578a905
Glecko <shevabogdan16@gmail.com>
2022-04-12 12:27:14 +0200

    strapi config update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,27 @@
 import type { GatsbyConfig } from "gatsby";
+import dotenv from 'dotenv'
+dotenv.config({
+  path: `.env`,
+});
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `decontex-gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: process.env.STRAPI_API_URL,
+        accessToken: process.env.STRAPI_TOKEN,
+        collectionTypes: ['User'],
+      }
+    },
+    "gatsby-plugin-styled-components"
+  ]
 };
+
+
 
 export default config;
