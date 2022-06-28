880dfbf4d458792a5a77e0d2c1e4ee0ca6e64f8d
kryptonumstudio <kryptonumstudio@gmail.com>
2022-06-28 14:39:44 +0200

    new spanish urls

--- src/constants/url.js
+++ src/constants/url.js
@@ -3,81 +3,94 @@ export const urls = {
         en: '/',
         de: '/de/',
         fr: '/fr/',
-        nl: '/nl/'
+        nl: '/nl/',
+        es: '/es/'
     },
     "Deco2fire": {
         en: '/deco2fire/',
         de: '/de/deco2fire/',
         fr: '/fr/deco2fire/',
-        nl: '/nl/deco2fire/'
+        nl: '/nl/deco2fire/',
+        es: '/es/deco2fire/'
     },
     "Decontomination": {
         en: '/decon-solution/',
         de: '/de/dekon-losung/',
         fr: '/fr/decon-solution/',
-        nl: '/nl/decon-solution/'
+        nl: '/nl/decon-solution/',
+        es: '/es/decon-solucion/'
     },
     "About": {
         en: '/about-us/',
         de: '/de/uber-uns/',
         fr: '/fr/about-us/',
-        nl: '/nl/about-us/'
+        nl: '/nl/about-us/',
+        es: '/es/sobre-nosotros/'
     },
     "Case Studies": {
         en: '/case-studies/',
         de: '/de/fallstudien/',
         fr: '/fr/case-studies/',
-        nl: '/nl/case-studies/'
+        nl: '/nl/case-studies/',
+        es: '/es/estudios-caso/'
     },
     "Lco2": { 
         en: '/pressurized-co2/', 
         de: '/de/druck-co2/', 
         fr: '/fr/pressurized-co2/', 
-        nl: '/nl/pressurized-co2/' 
+        nl: '/nl/pressurized-co2/',
+        es: '/es/co2-presurizado/'
     },
     "Faq": { 
         en: '/faq/', 
         de: '/de/faq/', 
         fr: '/fr/faq/', 
-        nl: '/nl/faq/' 
+        nl: '/nl/faq/',
+        es: '/es/pmf/'
     },
     "Contact": { 
         en: '/contact/', 
         de: '/de/kontakt/', 
         fr: '/fr/contact/',
-        nl: '/nl/contact/' 
+        nl: '/nl/contact/',
+        es: '/es/contact/'
     },
     "Freebies": { 
         en: '/download/', 
         de: '/de/download/', 
         fr: '/fr/download/', 
-        nl: '/nl/download/' 
+        nl: '/nl/download/',
+        es: '/es/descargar/'
     },
 
     "Privacy-police": { 
         en: '/cookie-policy/', 
         de: '/de/cookie-richtlinie/', 
         fr: '/fr/cookie-policy/', 
-        nl: '/nl/cookie-policy/' 
+        nl: '/nl/cookie-policy/',
+        es: '/es/cookie-policy/'
     },
     "Terms And Conditions": { 
         en: '/privacy-statement/', 
         de: '/de/datenschutzerklaerung/', 
         fr: '/fr/privacy-statement/', 
-        nl: '/nl/privacy-statement/' 
+        nl: '/nl/privacy-statement/',
+        es: '/es/privacy-statement/'
     },
 
     "Blog-archive": { 
         en: '/blog/', 
         de: '/de/blog/', 
         fr: '/fr/blog/', 
-        nl: '/nl/blog/' 
+        nl: '/nl/blog/',
+        es: '/es/blog/'
     },
 
     "News-archive": { 
         en: '/news/', 
         de: '/de/nachrichten/', 
         fr: '/fr/news/', 
-        nl: '/nl/news/' 
+        nl: '/nl/news/',
+        es: '/es/notitias/'
     },
 }
