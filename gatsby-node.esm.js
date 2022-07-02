9d0656223acd5a51ad69f50607614362b3400cdf
kryptonumstudio <kryptonumstudio@gmail.com>
2022-07-02 13:43:53 +0200

    mutilang 404

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -463,3 +463,18 @@ exports.createPages = async ({
     });
   });
 }
+
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+
+  if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
+    const oldPage = { ...page }
+
+    const langCode = page.path.split(`/`)[1]
+    page.matchPath = `/${langCode}/*`
+
+    deletePage(oldPage)
+    createPage(page)
+  }
+}
