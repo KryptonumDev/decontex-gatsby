a985d62a2df1d894d2c677af027ca788692d3c89
kryptonumstudio <kryptonumstudio@gmail.com>
2023-03-16 12:48:11 +0100

    change wp url

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: "gatsby-source-wordpress",
       options: {
-        url: 'http://decontex.adamchrapek.pl/graphql'
+        url: 'https://decontex.headlesshub.com/graphql'
       },
     },
     {
