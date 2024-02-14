6e70d8c2809059571a86f3ec2147342ba3a8f447
kryptonumstudio <kryptonumstudio@gmail.com>
2024-02-14 15:07:23 +0100

    fix header

--- src/components/parents/header.js
+++ src/components/parents/header.js
@@ -37,7 +37,7 @@ export default function Header({ location }) {
         }
     `)
 
-  const whiteWersionPages = ['/', '/de/', '/fr/', '/nl/', '/es/']
+  const whiteWersionPages = ['/', '/de/', '/fr/', '/nl/', '/es/', '/nb/']
 
   const locale = activeLanguage(location)
   const localeData = data.allWpPage.nodes.filter(el => el.language.slug === locale)
