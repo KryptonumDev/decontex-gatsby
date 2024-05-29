2265dcf15b7cc409b28b94b34e72dacac71a57c4
kryptonumstudio <kryptonumstudio@gmail.com>
2024-05-29 11:45:58 +0200

    fix misspelling

--- src/components/parents/leaflet-map.js
+++ src/components/parents/leaflet-map.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import countries from "./../../resources/countries.json";
@@ -43,7 +43,7 @@ export default function LeafletMap() {
           title: "Finland",
           email: "teknosafe@decontex.com",
           phone: ["+358 5 680 7700"],
-          address: "Decontex Finaland, Helsinki",
+          address: "Decontex Finland, Imatra",
           lat: 60.6699,
           lng: 24.9384,
           flag: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><mask id="a"><circle cx="256" cy="256" r="256" fill="#fff"/></mask><g mask="url(#a)"><path fill="#eee" d="M0 0h133.6l35.3 16.7L200.3 0H512v222.6l-22.6 31.7 22.6 35.1V512H200.3l-32-19.8-34.7 19.8H0V289.4l22.1-33.3L0 222.6z"/><path fill="#0052b4" d="M133.6 0v222.6H0v66.8h133.6V512h66.7V289.4H512v-66.8H200.3V0h-66.7z"/></g></svg>`,
